c9a9767d0bc0c4489c22714e4c44d98d6d22c49d
pekahero <pekahero@gmail.com>
2020-12-14 16:40:55 +0300

    Add files via upload

--- read-write.h
+++ read-write.h
@@ -14,4 +14,3 @@ using namespace std;
 char* read_from_binary_file(const string input_path, size_t& file_length);
 bool convert_and_write_to_file(const string output_path, char* message_binary, size_t& file_length);
 string bin_to_hex(char bin);
-string bin_to_dec(char bin);
